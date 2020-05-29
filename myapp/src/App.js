24a8bb0369db45b766ee251d0594f2a6b14cba85
samiran <ssamiran472@gmail.com>
2020-05-29 19:59:11 +0530

    react for token authentication

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Authentication from './components/logins'
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <Authentication />
+  );
+}
+
+export default App;
